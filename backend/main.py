979c026a1fc5bc5a3009c573b129e14d7edade3b
Aydi med amine <mohamedamine.aydi@numerys.io>
2022-09-07 17:48:18 +0100

    latest

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,16 @@
-from time import sleep
-from fastapi import FastAPI , Request, BackgroundTasks
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
 import os 
-
-stage = os.getenv('STAGE',"devStage")
-app = FastAPI(openapi_prefix=f"/{stage}")
-print(app.openapi_url)
+from routes import exemple
+stage = os.getenv('STAGE',"")
+app = FastAPI(openapi_prefix=f"/{stage}",description="Fast API Test application on aws", title="Serverless backend On AWS" )
 origins = [
     "*",
     "http://localhost:4000",
 ]
-
+app.include_router(exemple.router, prefix="/exemple")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -19,23 +18,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-def slp(time:int):
-    sleep(time)
-    print("done")
 
-@app.get("/")
-async def read_root(tasks : BackgroundTasks):
-    tasks.add_task(slp,3)
-    return {"Hello": "from fastapi newww "}
-@app.get("/a")
-def read_root2():
-    return {"Hello": "from fastapi newww "}
-@app.get("/app")
-async def read_main(request: Request):
-    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
+@app.get("/", tags=['MAIN'], response_class=JSONResponse)
+async def root():
+    return {"message": f"test API for {stage}"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
 
 handler = Mangum(app, lifespan="off")
